226f188579d990b0b5fe4a6882b6fb61de399a9a
Hossein Vahidi <Hn.vahidi@gmail.com>
2020-06-11 02:04:08 +0200

    Update Topological Sorting.cpp

--- 0 Graph Algorithms/Topological Sorting.cpp
+++ 0 Graph Algorithms/Topological Sorting.cpp
@@ -1,70 +1,58 @@
-//Hn.Vahidi@Gmail.com
+//too old for this stuff
 
-#include<iostream>
-#include<queue>
-#include<vector>
+#include<bits/stdc++.h>
 
 
-using  namespace std;
-const int maxN=1e5;
+using namespace std;
+const int maxN=1e6+100;
 
-vector<int> list, g[maxN+10];
-queue <int> q;
-int INdeg[maxN+10],label[maxN+10];
-int n, m;
+int n,m;
+int indeg[maxN];
+vector<int> g[maxN], topol;
+queue<int> Q;
 
 void input()
 {
-  cout<<" O( E+V)"<<endl<<"maxN=1e5   maxM=1e6"<<endl<<" INPUT VERTICES AND EDGES AND EDGES' DESCRIPTION"<<endl;
   cin>>n>>m;
   for(int i=1;i<=m;i++)
     {
       int k1,k2;
       cin>>k1>>k2;
       g[k1].push_back(k2);
+      indeg[k2]++;
     }
-
 }
 
-void INdeg_find()
+void topol_sort()
 {
   for(int i=1;i<=n;i++)
-    for(int j=0;j<g[i].size();j++)
-      INdeg[g[i][j]]++;
-
-  //sefr haro mindaze to queue
-  for(int i=1;i<=n;i++)
-    if(INdeg[i]==0)
-      q.push(i);
-
+    if(indeg[i]==0)
+      Q.push(i);
+  while(Q.empty()==false)
+    {
+      int v=Q.front();
+      Q.pop();
+      topol.push_back(v);
+      for(int i=0;i<g[v].size();i++)
+	{
+	  indeg[g[v][i]]--;
+	  
+	  if(indeg[g[v][i]]==0)
+	    Q.push(g[v][i]);
+	}
+    }
 }
-
-void show_list()
+void show()
 {
-  for(int i=0;i<list.size();i++)
-    cout<<list[i]<<" ";
+  for(int i=0;i<topol.size();i++)
+    cout<<topol[i]<<" ";
+  cout<<endl;
 }
 int main()
 {
   input();
-  INdeg_find();
-
-  int num=0;
-  while(!q.empty())
-    {
-      num++;
-      int v=q.front();
-      label[v]=num;
-      for(int i=0;i<g[v].size();i++){
-	INdeg[g[v][i]]--;
-	if(INdeg[g[v][i]]==0)
-	  q.push(g[v][i]);
-      }
-      list.push_back(v);
-      q.pop();
-      }
-
-  show_list();
-
+  topol_sort();
+  show();
+  
   return 0;
 }
